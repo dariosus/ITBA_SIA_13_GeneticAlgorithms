1410b8804cbb7a0114f9a0ec2a60af4fdb4a94f8
esneider <dariosn@gmail.com>
2013-06-05 19:40:09 -0300

    Universal

--- src/+algorithm/+ga/+selection/roulette.m
+++ src/+algorithm/+ga/+selection/roulette.m
@@ -1,27 +1,6 @@
 function population = roulette(k, oldPopulation)
 
-    F = 0;
-
-    for i = 1 : size(oldPopulation, 1)
-
-        F = F + oldPopulation{i}.fitness
-    end
-
-    population = cell(k, 1);
-
-    r = sort(rand(k, 1) * F, 'descend')
-
-    curR = 1;
-
-    for chromosome = 1 : size(oldPopulation, 1)
-
-        F = F - oldPopulation{chromosome}.fitness
-
-        while curR <= k && r(curR) > F
-
-            population{curR} = oldPopulation{chromosome};
-            curR = curR + 1
-        end
-    end
+    r = sort(rand(k, 1), 'descend');
+    population = algorithm.ga.selection.linealFitness(k, oldPopulation, r);
 end
 
