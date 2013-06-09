7266fae5c0bcf0d91d90c1a148b37d754e79c9d1
esneider <dariosn@gmail.com>
2013-06-09 18:35:14 -0300

    nicer linealFitness

--- src/+algorithm/+ga/+selection/rank.m
+++ src/+algorithm/+ga/+selection/rank.m
@@ -1,25 +1,9 @@
 function selected = rank(data, k, population)
 
-    [values indexes] = sort([population.fitness], 'descend');
+    [val pos] = sort([population.fitness], 'descend');
 
-    F = sum(indexes);
+    indexes = algorithm.ga.selection.linealFitness(k, [1 : numel(population)], rand(k, 1));
 
-    r = sort(rand(k, 1), 'descend') * F;
-
-    selected = repmat(population(1), k, 1);
-
-    rpos = 1;
-
-    for i = 1 : numel(population)
-
-        F = F - i;
-
-        while rpos <= k && r(rpos) > F
-
-            selected(rpos) = population(indexes(i));
-
-            rpos = rpos + 1;
-        end
-    end
+    selected = population(pos(indexes));
 end
 
--- src/+algorithm/+ga/+selection/universal.m
+++ src/+algorithm/+ga/+selection/universal.m
@@ -2,6 +2,8 @@ function selected = universal(data, k, population)
 
     r = [1 - rand() / k : -1 / k : 0];
 
-    selected = algorithm.ga.selection.linealFitness(k, population, r);
+    indexes = algorithm.ga.selection.linealFitness(k, [population.fitness], r);
+
+    selected = population(indexes);
 end
 
