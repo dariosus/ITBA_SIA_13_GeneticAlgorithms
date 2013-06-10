cb45d3fd92a8957d0c5e32178a4101294bd0d5dd
esneider <dariosn@gmail.com>
2013-06-10 11:16:45 -0300

    couple of extra tests

--- src/run_replacement.m
+++ src/run_replacement.m
@@ -11,4 +11,5 @@ i=340;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 's
 i=350;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'selection', @algorithm.ga.selection.mixed1, 'selection2', @algorithm.ga.selection.elite, 'replacement', @algorithm.ga.replacement.third);algorithm.ga.main(params);end
 i=360;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'selection', @algorithm.ga.selection.tourney, 'selection2', @algorithm.ga.selection.elite, 'replacement', @algorithm.ga.replacement.third);algorithm.ga.main(params);end
 i=370;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'selection', @algorithm.ga.selection.roulette, 'selection2', @algorithm.ga.selection.elite, 'replacement', @algorithm.ga.replacement.third);algorithm.ga.main(params);end
-
+i=400;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'selection', @algorithm.ga.selection.tourney, 'selection2', @algorithm.ga.selection.elite, 'replacement', @algorithm.ga.replacement.third, 'G', 1.2);algorithm.ga.main(params);end
+i=410;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'selection', @algorithm.ga.selection.roulette, 'selection2', @algorithm.ga.selection.elite, 'replacement', @algorithm.ga.replacement.third, 'G', 1.2);algorithm.ga.main(params);end
