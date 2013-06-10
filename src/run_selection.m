91e5d1793973715fa37946ddd6d7d350079eaba2
cynthiaa <cynthiaa@live.com.ar>
2013-06-10 03:00:09 -0300

    run selection

--- src/run_selection.m
+++ src/run_selection.m
@@ -5,4 +5,5 @@ i=80;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'se
 i=90;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'selection', @algorithm.ga.selection.mixed2);algorithm.ga.main(params);end
 i=100;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'selection', @algorithm.ga.selection.roulette);algorithm.ga.main(params);end
 i=110;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'selection', @algorithm.ga.selection.tourney);algorithm.ga.main(params);end
+i=180;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'selection', @algorithm.ga.selection.boltzmann);algorithm.ga.main(params);end
 
