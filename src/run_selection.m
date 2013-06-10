4e1d3f3b5d216f9f3fa8ca53d0bff8c673f0b2ef
cynthiaa <cynthiaa@live.com.ar>
2013-06-10 03:20:26 -0300

    Testing mixed1 and mixed2 -

--- src/run_selection.m
+++ src/run_selection.m
@@ -6,7 +6,6 @@ i=90;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'se
 i=100;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'selection', @algorithm.ga.selection.roulette);algorithm.ga.main(params);end
 i=110;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'selection', @algorithm.ga.selection.tourney);algorithm.ga.main(params);end
 i=180;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'selection', @algorithm.ga.selection.boltzmann);algorithm.ga.main(params);end
-
 i=200;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'selection', @algorithm.ga.selection.mixed1, 'a', 0.9);algorithm.ga.main(params);end
 i=210;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'selection', @algorithm.ga.selection.mixed2, 'a', 0.9);algorithm.ga.main(params);end
 i=220;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'selection', @algorithm.ga.selection.mixed1, 'a', 0.1);algorithm.ga.main(params);end
