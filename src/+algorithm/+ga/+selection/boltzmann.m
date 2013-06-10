74cb30a11d5a630cfb7cb91efcc0fae0e2f0301c
cynthiaa <cynthiaa@live.com.ar>
2013-06-10 01:01:30 -0300

    run_network

--- src/+algorithm/+ga/+selection/boltzmann.m
+++ src/+algorithm/+ga/+selection/boltzmann.m
@@ -1,10 +1,11 @@
 function selected = boltzmann(data, k, population)
 
-    T = (1 - data.alg.generation / data.const.maxGeneration) * data.const.tempFactor;
+    T = (1 - data.alg.generation / data.const.maxGenerations) * data.const.tempFactor;
 
     expValues = exp([population.fitness] ./ T);
 
     indexes = algorithm.ga.selection.linealFitness(k, expValues, rand(k, 1));
+
     selected = population(indexes);
 end
 
