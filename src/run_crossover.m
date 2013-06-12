658a653208c15b68a0aaec784e176d721b1f9974
esneider <dariosn@gmail.com>
2013-06-12 01:49:32 -0300

    more tests

--- src/run_crossover.m
+++
@@ -1,7 +0,0 @@
-R = 3;
-
-i=10;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'crossover', @algorithm.ga.crossover.annular);algorithm.ga.main(params);end
-i=20;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'crossover', @algorithm.ga.crossover.doublePoint);algorithm.ga.main(params);end
-i=30;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'crossover', @algorithm.ga.crossover.singlePoint);algorithm.ga.main(params);end
-i=40;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'crossover', @algorithm.ga.crossover.uniform);algorithm.ga.main(params);end
-
--- src/run_mutation.m
+++
@@ -1,7 +0,0 @@
-R = 3;
-
-i=50;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'mutation', @algorithm.ga.mutation.chromosome);algorithm.ga.main(params);end
-i=60;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'mutation', @algorithm.ga.mutation.locus);algorithm.ga.main(params);end
-i=150;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'nonUniformMutation', true, 'mutation', @algorithm.ga.mutation.chromosome);algorithm.ga.main(params);end
-i=160;for j=1:R; i=i+1;params=struct('path', strcat('output',num2str(i),'/'), 'nonUniformMutation', true, 'mutation', @algorithm.ga.mutation.locus);algorithm.ga.main(params);end
-
