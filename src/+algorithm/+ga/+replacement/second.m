196385fc07a73b8cbc28e52144a1f630a1b8d77a
cynthiaa <cynthiaa@live.com.ar>
2013-06-12 08:45:53 -0300

    population

--- src/+algorithm/+ga/+replacement/second.m
+++ src/+algorithm/+ga/+replacement/second.m
@@ -1,6 +1,6 @@
 function data = second(data, k)
 
-    population = data.const.selection2(data, k, false);
+    population = data.const.selection2(data, k, data.alg.population, false);
 
     [data population] = algorithm.ga.replacement.spawn(data, population(randperm(k)));
 
